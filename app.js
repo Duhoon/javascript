3c8c42c9f1d41dcb2e9484a82dd19adfe4ae069a
Kim-byoungkwan <bg0307@ajou.ac.kr>
2019-07-24 18:02:22 +0900

    7.24 real final commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 
 router.get('/cookie', function (req, res) {
-res.sendFile(path.join(__dirname + '/views/byoungkwan'));
+res.sendFile(path.join(__dirname + '/views/byoungkwan/cookies.html'));
 //__dirname : It will resolve to your project folder.
 });
 
